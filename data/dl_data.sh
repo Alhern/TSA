af36a500a9d221b4ab262c3df206b9c77d584b67
Alhern <23422910+Alhern@users.noreply.github.com>
2021-01-13 17:31:14 +0100

    sh script to dl Sentiment140

---
+++ data/dl_data.sh
@@ -0,0 +1,7 @@
+# Downloads the required dataset 'training.1600000.processed.noemoticon.csv'
+# If the link doesn't work anymore, try https://docs.google.com/file/d/0B04GJPshIjmPRnZManQwWEdTZjg/edit
+
+wget http://cs.stanford.edu/people/alecmgo/trainingandtestdata.zip
+unzip trainingandtestdata.zip
+rm trainingandtestdata.zip
+rm testdata.manual.2009.06.14.csv
