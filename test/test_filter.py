b46740c3cf43816441580ef889a3dc926cec4b33
Alhern <23422910+Alhern@users.noreply.github.com>
2021-01-25 23:04:38 +0100

    minor PEP8 fix

--- test/test_filter.py
+++ test/test_filter.py
@@ -2,6 +2,7 @@ import filter
 import unittest
 from unittest.mock import patch
 
+
 @patch('filter.stoplist', filter.create_stoplist(punctuation=True, extra_punctuation=False, collection_w=False, stopword_list=False))
 
 class TestFilter(unittest.TestCase):
@@ -10,7 +11,6 @@ class TestFilter(unittest.TestCase):
         expected_result = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
         given_result = filter.create_stoplist(punctuation=True, extra_punctuation=False, collection_w=False, stopword_list=False)
         self.assertEqual(given_result, expected_result)
-
 
     def test_filter_stopwords(self):
         test_case = ['coucou', '!', 'un', ',', 'test', '.']
