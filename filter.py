ffcd1720d91a7cfd00759f8ccef6f47d160e8acd
Alhern <23422910+Alhern@users.noreply.github.com>
2021-01-25 16:06:05 +0100

    added create_stoplist()

--- filter.py
+++ filter.py
@@ -20,10 +20,14 @@ from nltk.corpus import stopwords
 # ------------------------------------------
 
 # collection_words sont les mots que l'on utilise pour dataminer nos tweets, ces mots de collection
-# vont alors se retrouver dans tous ou la majorité des tweets. On va donc les enlever parce qu'ils n'apportent rien d'intéressant pour notre analyse :
+# vont alors se retrouver dans tous ou la majorité des tweets. On va donc les enlever parce qu'ils n'apportent rien d'intéressant pour notre analyse, exemples :
 
 collection_words_cp = ['#cyberpunk2077', 'cyberpunk', '2077', '#cyberpunk', '@cyberpunkgame']
 collection_words_trump = ['impeached', 'impeachment']
+
+# Mettez les collection words relatifs à votre query dans la liste suivante :
+
+collection_words = []
 
 
 # string.punctuation n'enlève pas toutes les ponctuations trouvé dans le dataset,
@@ -39,13 +43,27 @@ extra_punct = ['…', '...', '’', '..', '️']
 stopwords = stopwords.words('english')
 
 
-# La stoplist va enfin être composée de plusieurs éléments indésirables :
+# La stoplist va enfin être composée de plusieurs éléments indésirables, au choix :
 
-stoplist = list(string.punctuation) + extra_punct + collection_words_cp
+def create_stoplist(punctuation=True, extra_punctuation=True, collection_w=True, stopword_list=False):
+    stoplist = []
+    if punctuation:
+        stoplist += list(string.punctuation)
+    if extra_punctuation:
+        stoplist += extra_punct
+    if collection_w:
+        stoplist += collection_words
+    if stopword_list:
+        stoplist += stopwords
+    return stoplist
+
+# On l'initialise avec les éléments que l'on souhaite filtrer ou pas :
+
+stoplist = create_stoplist(punctuation=True, extra_punctuation=True, collection_w=True, stopword_list=False)
 
 # ------------------------------------------
 
-# Les stopwords vont être filtré de notre liste de tokens (on en profite pour virer les chiffres) :
+# Les stopwords vont être filtrés de notre liste de tokens :
 
 def filter_stopwords(tokens):
-    return [t for t in tokens if not t in stoplist and not t.isdigit()]
+    return [t for t in tokens if not t in stoplist]
