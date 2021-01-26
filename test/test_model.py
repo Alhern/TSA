6fde7446bd8ad3abaaa6f5a00b84092cb7547167
Alhern <23422910+Alhern@users.noreply.github.com>
2021-01-26 22:01:36 +0100

    added unit tests for model.py

--- test/test_model.py
+++ test/test_model.py
@@ -2,7 +2,11 @@ import model
 import numpy as np
 import unittest
 from unittest.mock import patch
+import tempfile
+import os
 import pandas
+import io
+import sys
 
 
 filename = 'test_sentiment.csv'
@@ -23,10 +27,28 @@ class TestModel(unittest.TestCase):
 
     @patch('model.MIN_COUNT', 1)
     def test_w2vmodel_builder(self):
-        test_data = np.array(model.postprocess(test_df).tokens)
-        w2vmodel = model.w2vmodel_builder(test_data)
-        similar = w2vmodel.wv.most_similar('long')
-        self.assertTrue(similar)
+        with patch('sys.stdout', new=io.StringIO()):
+            test_data = np.array(model.postprocess(test_df).tokens)
+            w2vmodel = model.w2vmodel_builder(test_data)
+            similar = w2vmodel.wv.most_similar('long')
+            self.assertTrue(similar)
+            sys.stdout = sys.__stdout__
+
+    @patch('model.MIN_COUNT', 1)
+    def test_save_load_w2vmodel(self):
+        test_tmp_model = os.path.join(tempfile.gettempdir(), 'test_word2vec_model.tmp')
+
+        with patch('sys.stdout', new=io.StringIO()):
+            test_data = np.array(model.postprocess(test_df).tokens)
+            w2vmodel = model.w2vmodel_builder(test_data)
+            test_vocab_len = len(w2vmodel.wv.vocab)
+            model.save_w2vmodel(w2vmodel, test_tmp_model)
+
+            loaded_model = model.load_w2vmodel(test_tmp_model)
+            expected_vocab_len = len(loaded_model.wv.vocab)
+
+            self.assertEqual(test_vocab_len, expected_vocab_len)
+            sys.stdout = sys.__stdout__
 
 
 if __name__ == "__main__":
