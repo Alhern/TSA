bf7a8aa4ec16ff06bdd8a4152fa58b41a5ccec6b
Alhern <23422910+alhern@users.noreply.github.com>
2022-06-29 22:42:25 +0200

    (very minor) updated example

--- test/test_model.py
+++ test/test_model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6aa983858eeb142e63d4c8280db0b6002f784e94d0ff74e49f5c28a98242ba9
+oid sha256:407bf5a0159b5d00413f9969112d18e63bd46c9305a85f712749f4dc87763bff
 size 2186
