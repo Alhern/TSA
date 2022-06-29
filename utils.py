d662890598eba22dec1bbc89ff5bcb183a321c2b
Alhern <23422910+alhern@users.noreply.github.com>
2022-06-29 17:09:22 +0200

    modified parameters and added a fail safe

--- utils.py
+++ utils.py
@@ -44,16 +44,24 @@ def load_modeljson(filename, weights):
 # le fichier est une suite de listes contenant des chaînes.
 # valid_json va transformer ce fichier en fichier json valide.
 
-def valid_json(file, new_file):
-    with open(file) as f, open(new_file, 'w', encoding='utf-8') as valid_file:
-        data = json.loads("[" + f.read().replace("}{", "},\n{") + "]")  # magie
-        json.dump(data, valid_file, ensure_ascii=False, indent=4)
-    print("%s created." % new_file)
+def valid_json(file):
+    new_file = "valid_" + file
+    try:
+        with open(file) as f, open(new_file, 'w', encoding='utf-8') as valid_file:
+            data = json.loads("[" + f.read().replace("}{", "},\n{") + "]")  # magie
+            json.dump(data, valid_file, ensure_ascii=False, indent=4)
+            print("%s created." % new_file)
+    except IOError as e:
+        print(f"File not found: {e}")
+        return None
 
 
 # Chargement du fichier json à partir de l'ordi
 
 def read_json(file):
     with open(file, 'r', encoding='utf-8') as f:
-        data = json.load(f)
-        return data
+        try:
+            return json.load(f)
+        except ValueError as e:
+            print(f"Invalid JSON file: {e}")
+            return None
