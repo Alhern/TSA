cd8dffa49de54476195cbfa6cb14311f1fbd454f
Alhern <23422910+alhern@users.noreply.github.com>
2022-06-29 22:40:13 +0200

    removed useless librairies

--- test/test_analyzer.py
+++ test/test_analyzer.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1eff02222bc4cbf5cc5350dbf3e171fa3b97d9604c1ef5a659ead4e1a7e34550
-size 957
+oid sha256:7b22bfc216b81f8d6616db4af3332285bf6c5584babb815841489e5710475b7d
+size 931
