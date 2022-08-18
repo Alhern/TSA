b7bbaead4af8ae73e2522b760caa4225c39fc34b
Alhern <23422910+alhern@users.noreply.github.com>
2022-08-18 14:27:13 +0200

    permanent menu

--- analyzer.py
+++ analyzer.py
@@ -134,68 +134,70 @@ def main():
 
     print(art)
     print("---Welcome to TSA---")
-    print("What would you like to do?\n")
-    print("\t1) Mine tweets")
-    print("\t2) Validate a json file")
-    print("\t3) Analyze a corpus of tweets")
-    print("\t4) Find the most common words in a corpus of tweets")
-    print("\t5) Predict a string")
-    print("\t6) Quit")
+    while True:
+        print("---------------------------------------------------")
+        print("What would you like to do?\n")
+        print("\t1) Mine tweets")
+        print("\t2) Validate a json file")
+        print("\t3) Analyze a corpus of tweets")
+        print("\t4) Find the most common words in a corpus of tweets")
+        print("\t5) Predict a string")
+        print("\t6) Quit")
+        print("---------------------------------------------------")
 
-    choice = input("\nEnter your choice (1-6): ")
+        choice = input("\nEnter your choice (1-6): ")
 
-    if choice == "1":
-        miner.main()
+        if choice == "1":
+            miner.main()
 
-    elif choice == "2":
-        path = input("Enter the path to the json file: ")
-        try:
-            valid_json(path)
+        elif choice == "2":
+            path = input("Enter the path to the json file: ")
+            try:
+                valid_json(path)
+            except Exception as e:
+                print(e)
+                exit(1)
+
+        # /!\ On doit avoir passé le fichier obtenu avec miner.py dans valid_json() avant de lancer cette fonction
+        elif choice == "3":
+
+            # On charge le nouveau fichier json avec read_json()
+            path = input("Enter the path to the JSON corpus you want to analyze: ")
+            extra = input("Would you like to visualize extra data? (y/n): ")
+            try:
+                data = read_json(path)
+                tokens = tokenize_tweets(data)
+                # Time to predict, on va calculer les taux de sentiments positifs et négatifs se trouvant dans notre corpus de tweets:
+                dataset_prediction(tokens)
+
+                if extra.lower() == "y":
+                    plots.all_plots(path)
+            except Exception as e:
+                print(e)
+                exit(1)
+
+        elif choice == "4":
+            num = int(input("Enter the number of the most common words you want to see: "))
+            path = input("Enter the path to the JSON corpus you want to analyze: ")
+            try:
+                data = read_json(path)
+                tokens = tokenize_tweets(data)
+                most_common_words(tokens, num)
+            except Exception as e:
+                print(e)
+                exit(1)
+
+        elif choice == "5":
+            string = input("Enter the string you want to predict: ")
+            predict_this(string)
+
+        elif choice == "6":
+            print("Bye!")
             exit(0)
-        except Exception as e:
-            print(e)
+
+        else:
+            print("Invalid choice\nPlease try entering 1, 2, 3, 4 or 5.")
             exit(1)
-
-    # /!\ On doit avoir passé le fichier obtenu avec miner.py dans valid_json() avant de lancer cette fonction
-    elif choice == "3":
-
-        # On charge le nouveau fichier json avec read_json()
-        path = input("Enter the path to the JSON corpus you want to analyze: ")
-        extra = input("Would you like to visualize extra data? (y/n): ")
-        try:
-            data = read_json(path)
-            tokens = tokenize_tweets(data)
-            # Time to predict, on va calculer les taux de sentiments positifs et négatifs se trouvant dans notre corpus de tweets:
-            dataset_prediction(tokens)
-
-            if extra.lower() == "y":
-                plots.all_plots(path)
-        except Exception as e:
-            print(e)
-            exit(1)
-
-    elif choice == "4":
-        num = int(input("Enter the number of the most common words you want to see: "))
-        path = input("Enter the path to the JSON corpus you want to analyze: ")
-        try:
-            data = read_json(path)
-            tokens = tokenize_tweets(data)
-            most_common_words(tokens, num)
-        except Exception as e:
-            print(e)
-            exit(1)
-
-    elif choice == "5":
-        string = input("Enter the string you want to predict: ")
-        predict_this(string)
-
-    elif choice == "6":
-        print("Bye!")
-        exit(0)
-
-    else:
-        print("Invalid choice\nPlease try entering 1, 2, 3, 4 or 5.")
-        exit(1)
 
 
 if __name__ == "__main__":
